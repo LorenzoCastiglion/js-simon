c38c8cd04cf9352091dfe5b6090df1cd9593f3ef
Lorenzo Castiglion <lorenzo.castiglion@hotmail.it>
2022-10-19 14:50:27 +0200

    funzione genera num inserita in bottone e timef(x)

--- JS/script.js
+++ JS/script.js
@@ -9,25 +9,38 @@ Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei num
 
 const memoryHtml = document.getElementById('memory');
 
-const btn = document.querySelector('button');
+const btnGen = document.getElementById('genera')
+
+const btnSend = document.getElementById('send');
 
 let num = [];
 
 let numNum = 5;
 
+// bottone che avvia il gioco e genera i numeri
+btnGen.addEventListener('click',
+
 // generatore dei cinque numeri
-while (num.length < numNum){
-    let generate = randomNumber(1,100);
-
-// controllo numeri generati per non pushare doppioni nell'array
-    if(!num.includes(generate)){
-        num.push(generate)
+function genNum () {
+    while (num.length < numNum){
+        let generate = randomNumber(1,100);
+    
+    // controllo numeri generati per non pushare doppioni nell'array
+        if(!num.includes(generate)){
+            num.push(generate)
+        }
     }
-}
+    
+    // printo l'array numrndm nel DOM
+    memoryHtml.innerHTML = num;
+
+    // setto una arrow funct che mi cancella il print array num nel DOM
+setTimeout(() => {
+    memoryHtml.innerHTML= '';
+  }, 3000)
+})
 
 
-// stampo i nnumeri casuali nel DOM
-memoryHtml.innerHTML = num;
 
 
 
