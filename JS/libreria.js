2fb7015a5cbcd0a003a138f4c0a8a61f85767c52
Lorenzo Castiglion <lorenzo.castiglion@hotmail.it>
2022-10-19 12:44:08 +0200

    impostazione lavoro

---
+++ JS/libreria.js
@@ -0,0 +1,37 @@
+function randomNumber(min, max) {
+    //console.log(arguments);//
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+function isEven(num){
+    if(num % 2 === 0){
+        return true;
+    } else {
+        return false;
+    }
+}
+function notificationError(msgError){
+    const alerta = document.createElement('div');
+    alerta.className='alert alert-danger';
+    alerta.innerHTML = msgError;
+    return alerta;
+}
+function removeFirstNotification(){
+    const alertToRemove = document.querySelector('.alert');
+    console.log(alertToRemove);
+   if(alertToRemove) alertToRemove.remove();
+}
+
+// const parolaLen = parola.length;
+// let parolaGirata = parola.split('').reverse().join('');
+
+function palindrome(str) {
+    let parola = str.toLowerCase();
+    let parolaGirata = parola.split('').reverse().join('');
+    return parolaGirata === parola;
+}
+// for(let i = parolaLen - 1; i >= 0; i-- ){
+//     parolaGirata += parola.charAt(i);
+// }
+// console.log(parolaGirata);
+
+
